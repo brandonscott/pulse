bd66e2cc7adc290cf7cb45d14241c310156bbb06
Jimmy <jimmyjamesbaldwin@gmail.com>
2014-05-07 16:28:38 +0100

    Integrated with the Pubnub API (thanks to help from Brandon)

--- src/com/mercury/pulse/activities/ServerInfoActivity.java
+++ src/com/mercury/pulse/activities/ServerInfoActivity.java
@@ -4,12 +4,18 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+
 import com.example.pulse.R;
 import com.mercury.pulse.views.PieChartView;
 import com.mercury.pulse.views.SmallPieChartView;
+import com.pubnub.api.Callback;
+import com.pubnub.api.Pubnub;
+import com.pubnub.api.PubnubException;
+import com.pubnub.api.PubnubError;
 
 public class ServerInfoActivity extends Activity {
 	
+	private Pubnub pubnub = new Pubnub("pub-c-18bc7bd1-2981-4cc4-9c4e-234d25519d36", "sub-c-5782df52-d147-11e3-93dd-02ee2ddab7fe");
 	private PieChartView					mPieChart;
 	private SmallPieChartView 				mPieChart2, mPieChart3;
 	
@@ -26,6 +32,49 @@ public class ServerInfoActivity extends Activity {
 		mPieChart3 = (SmallPieChartView) findViewById(R.id.stats_piechart3);
 
 		updateData();
+		pubnub();
+	}
+	
+	public void pubnub() {
+		 try {
+			   pubnub.subscribe("pulses", new Callback() {
+			 
+			       @Override
+			       public void connectCallback(String channel, Object message) {
+			           System.out.println("SUBSCRIBE : CONNECT on channel:" + channel
+			                      + " : " + message.getClass() + " : "
+			                      + message.toString());
+			       }
+			 
+			       @Override
+			       public void disconnectCallback(String channel, Object message) {
+			           System.out.println("SUBSCRIBE : DISCONNECT on channel:" + channel
+			                      + " : " + message.getClass() + " : "
+			                      + message.toString());
+			       }
+			 
+			       public void reconnectCallback(String channel, Object message) {
+			           System.out.println("SUBSCRIBE : RECONNECT on channel:" + channel
+			                      + " : " + message.getClass() + " : "
+			                      + message.toString());
+			       }
+			 
+			       @Override
+			       public void successCallback(String channel, Object message) {
+			           System.out.println("SUBSCRIBE : " + channel + " : "
+			                      + message.getClass() + " : " + message.toString());
+			       }
+			 
+			       @Override
+			       public void errorCallback(String channel, PubnubError error) {
+			           System.out.println("SUBSCRIBE : ERROR on channel " + channel
+			                      + " : " + error.toString());
+			       }
+			     }
+			   );
+			 } catch (PubnubException e) {
+			   System.out.println(e.toString());
+			 }
 	}
 
 	
@@ -36,10 +85,6 @@ public class ServerInfoActivity extends Activity {
 	public class UpdateData extends AsyncTask<Void, Void, Void>{
 		@Override
 		protected void onPreExecute(){
-			//mProgress.setVisibility(ProgressBar.VISIBLE);
-			//mAverageReturn.setVisibility(TextView.GONE);
-			//mAverageReturnLbl.setVisibility(TextView.GONE);
-			//mPieChartLbl.setVisibility(TextView.GONE);
 			mPieChart.setVisibility(View.GONE);
 			mPieChart2.setVisibility(View.GONE);
 			mPieChart3.setVisibility(View.GONE);
@@ -52,15 +97,6 @@ public class ServerInfoActivity extends Activity {
 
 		@Override
 		protected void onPostExecute(Void no){
-			//mProgress.setVisibility(ProgressBar.INVISIBLE);
-			//mAverageReturnLbl.setVisibility(TextView.VISIBLE);
-			//mAverageReturnLbl.setText("No data could be generated");
-			//mAverageReturn.setText("Days Without Issue");
-			//mAverageReturnLbl.setText("Days Without Issue");
-			//mAverageReturnLbl.setVisibility(TextView.VISIBLE);
-			//mAverageReturn.setText(String.format(getActivity().getResources().getString(R.string.stats_averageloan_value),
-					//mAverageReturnAmount));
-			//mPieChartLbl.setVisibility(TextView.VISIBLE);
 			mPieChart.setData(25);
 			mPieChart2.setData(76);
 			mPieChart3.setData(83);
