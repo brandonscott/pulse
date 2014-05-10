0d4cf16a585f1c92b58c3b24395c3ab04c8bc985
Jimmy <jimmyjamesbaldwin@gmail.com>
2014-05-10 16:34:21 +0100

    Implemented tab views on the AboutActivity and updated content

--- src/com/mercury/pulse/activities/AboutActivity.java
+++ src/com/mercury/pulse/activities/AboutActivity.java
@@ -1,35 +1,79 @@
 package com.mercury.pulse.activities;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.text.Html;
-import android.view.Gravity;
-import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 
 import com.example.pulse.R;
+import com.mercury.pulse.adapters.TabsPagerAdapter;
 
-public class AboutActivity extends Activity {
-	
-	private TextView mTextViewBoringStuff;
-	
+public class AboutActivity extends FragmentActivity implements
+ActionBar.TabListener {
+
+	private ViewPager viewPager;
+	private TabsPagerAdapter mAdapter;
+	private ActionBar actionBar;
+	// Tab titles
+	private String[] tabs = { "About Pulse", "About Us", "References" };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
-		
-		init();
+
+		// Initilization
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getActionBar();
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+		viewPager.setAdapter(mAdapter);
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);		
+
+		// Adding Tabs
+		for (String tab_name : tabs) {
+			actionBar.addTab(actionBar.newTab().setText(tab_name)
+					.setTabListener(this));
+		}
+
+		/**
+		 * on swiping the viewpager make respective tab selected
+		 * */
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int position) {
+				// on changing the page
+				// make respected tab selected
+				actionBar.setSelectedNavigationItem(position);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
 	}
-	
-	public void init() {
-		mTextViewBoringStuff = (TextView)findViewById(R.id.aboutfragment_boringstuff_references);
-		mTextViewBoringStuff.setText(Html.fromHtml(""
-				+ "<center><strong>License</strong></center><br />"
-				+ "Pulse &copy; is released under XXX license.<br /><br />"
-				+ "<strong>References</strong><br />"
-				+ "Pulse implements the following 3rd party libraries & resources:<br />"
-				+ "RoundedImageView.java,<br />"
-				+ "Solar System Icons by Dan Wiersema,"));	
-		mTextViewBoringStuff.setGravity(Gravity.CENTER);
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// on tab selected
+		// show respected fragment view
+		viewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 	}
 
 }
